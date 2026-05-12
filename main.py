91d1cc453be7b830306569f28898f09b46757272
Adam Ahmed <aadam.2012legend@gmail.com>
2026-05-12 17:28:47 +0530

    2nd part of day 6 class

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import streamlit as st
 
-st.title("My 1st Streamlit App")
-st.write("Hello, World!")
+st.title("Calculator")
 
-st.markdown("This is a streamlit app created by Adam Ahmed **the great**.")
-st.markdown("if you click this button below, you are smart.")
-if st.button("Click me!"):
-    st.write("Just kidding u just clicked a button, you are not smart. Also, I may or may not have downlaoded a **virus on your computer**, so be careful. If u want to delete it **click below**")
-    if st.button("Delete virus"):
-        st.write("**LOL U CLICKED A BUTTON AGAIN LOL. THERE WAS NO VIRUS IN THE FIRST PLACE, I WAS JUST JOKING.**")
-        
-st.markdown("Made *without* love by Adam Ahmed.")
+a = st.number_input("Enter first number")
+b = st.number_input("Enter second number")
+c = st.selectbox("Pick operation", ["+", "-", "*", "/"])
+
+if st.button("Calculate"):
+    if c == "+":
+        d = a + b
+    elif c == "-":
+        d = a - b
+    elif c == "*":
+        d = a * b
+    elif c == "/":
+        d = a / b
+    
+    st.write(f"Result: {d}")
